74c7b47caaf1cc4aa0832496799ecb7605e1fb8d
wankh <wankh@MSI>
2022-04-25 14:38:17 +0530

    By Ram25

--- GitDemo/src/A.java
+++ GitDemo/src/A.java
@@ -1,9 +1,16 @@
 
 public class A 
 {
+	
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello  A ");
+		A a= new A();
+		a.m1();
 	}
 
 }
